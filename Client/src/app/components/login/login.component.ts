8ea74fc0b77cf48b4690d27fc5c90eef7b239265
Nguyen Ba Quyen <baquyennguyen94@gmail.com>
2023-07-10 23:48:58 +0700

    create Login form and Signup form, update validate for 2 forms

---
+++ Client/src/app/components/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForm';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  loginForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(){
+    this.loginForm = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  onLogin(){
+    if(this.loginForm.valid){
+
+    }else{
+      console.log("Form is not valid");
+
+      ValidateForm.validateAllFormFields(this.loginForm);
+      alert("Your form is invalid");
+    }
+  }
+}
---
+++ Client/src/app/components/signup/signup.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForm';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  type: string = "password";
+  isText: boolean = false;
+  eyeIcon: string = "fa-eye-slash";
+  signUpForm!: FormGroup;
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  hideShowPass(){
+    this.isText = !this.isText;
+    this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
+    this.isText ? this.type = "text" : this.type = "password";
+  }
+
+  createForm(){
+    this.signUpForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', Validators.required],
+      userName: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSignUp(){
+    if(this.signUpForm.valid){
+
+    }else{
+      console.log("Form is not valid");
+
+      ValidateForm.validateAllFormFields(this.signUpForm);
+      alert("Your form is invalid");
+    }
+  }
+}
